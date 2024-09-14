067201bd28bfe8735762bca9618e887d92d3b22f
NotAlexNoyle <alexnoyle@icloud.com>
2024-09-14 04:41:57 +0000

    Remove test code

--- src/main/java/plugin/TemplateOG.java
+++ src/main/java/plugin/TemplateOG.java
@@ -10,7 +10,6 @@ public class TemplateOG extends JavaPlugin {
 		plugin = this;
 
 		getServer().getPluginManager().registerEvents(new Listeners(), this);
-		//registerExpansion();
 
 	}
 
@@ -19,30 +18,5 @@ public class TemplateOG extends JavaPlugin {
 		return plugin;
 
 	}
-
-	/*public void registerExpansion() {
-
-		// Create <diamondbankog_balance> placeholder.
-		Expansion expansion = Expansion.builder("diamondbankog").globalPlaceholder("balance", (queue, ctx) -> {
-
-			CompletableFuture<Double> balanceFuture = DiamondBankOG.getPlayerBalance(player.getUniqueId(), BalanceType.ALL);
-			double balance = 0.0;
-			try {
-			    balance = balanceFuture.get(); // This might block the current thread
-			    if (balance != null) {
-			        System.out.println("Player balance: " + balance);
-			    } else {
-			        System.out.println("Player balance could not be retrieved.");
-			    }
-			} catch (InterruptedException | ExecutionException e) {
-			    System.out.println("Error getting balance: " + e.getMessage()); 
-			}
-
-			return Tag.selfClosingInserting(Component.text(balance));
-		}).build();
-
-		expansion.register();
-
-	}*/
 
 }
