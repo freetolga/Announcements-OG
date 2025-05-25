fca92a5b0d2fa745926d58a4e540bc6f74a7baab
NotAlexNoyle <alexnoyle@icloud.com>
2025-05-25 17:45:54 -0400

    Test build for DiamondBank-OG

--- src/main/java/plugin/Listeners.java
+++ src/main/java/plugin/Listeners.java
@@ -1,10 +1,15 @@
 package plugin;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import net.trueog.diamondbankog.DiamondBankOG;
+import net.trueog.diamondbankog.PostgreSQL;
 import net.trueog.utilitiesog.UtilitiesOG;
 
 public class Listeners implements Listener {
@@ -12,13 +17,13 @@ public class Listeners implements Listener {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onBlockBreak(BlockBreakEvent event) {
 
-		//CompletableFuture<PostgreSQL.PlayerBalance> asyncPlayerBalance = DiamondBankOG.getApi().getPlayerBalance(event.getPlayer().getUniqueId(), PostgreSQL.BalanceType.ALL);
+		CompletableFuture<PostgreSQL.PlayerBalance> asyncPlayerBalance = DiamondBankOG.getApi().getPlayerBalance(event.getPlayer().getUniqueId(), PostgreSQL.BalanceType.ALL);
 
-		//try {
+		try {
 
-		//PostgreSQL.PlayerBalance balanceResult = asyncPlayerBalance.get();
+		PostgreSQL.PlayerBalance balanceResult = asyncPlayerBalance.get();
 
-		//double totalBalance = balanceResult.getBankBalance() + balanceResult.getEnderChestBalance() + balanceResult.getInventoryBalance();
+		double totalBalance = balanceResult.getBankBalance() + balanceResult.getEnderChestBalance() + balanceResult.getInventoryBalance();
 
 		// Send a message to the player with their balance.
 
@@ -27,12 +32,12 @@ public class Listeners implements Listener {
 		UtilitiesOG.logToConsole("[Template-OG]", "The player: " + event.getPlayer() + "'s <aqua>balance</aqua> is: " + 5 + "&B Diamonds");
 
 
-		//}
-		//catch (InterruptedException | ExecutionException error) {
+		}
+	    catch (InterruptedException | ExecutionException error) {
 
-		//	UtilitiesOG.logToConsole(TemplateOG.getPlugin(), "ERROR: The player: " + event.getPlayer() + "'s balance could not be fetched! ERROR: " + error.getMessage());
+			UtilitiesOG.logToConsole("[Template-OG]", "ERROR: The player: " + event.getPlayer() + "'s balance could not be fetched! ERROR: " + error.getMessage());
 
-		//}
+		}
 
 	}
 
