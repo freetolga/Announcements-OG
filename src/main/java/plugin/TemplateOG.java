b94060639ff0d9171bb8c50cae6404672570ab52
Sven <68018116+SKBotNL@users.noreply.github.com>
2025-05-26 14:37:51 +0200

    Use new DiamondBank-OG API

--- src/main/java/plugin/TemplateOG.java
+++ src/main/java/plugin/TemplateOG.java
@@ -5,18 +5,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class TemplateOG extends JavaPlugin {
 
 	private static TemplateOG plugin;
+	private static DiamondBankAPI diamondBankAPI;
+	
 	public void onEnable() {
-
 		plugin = this;
 
 		getServer().getPluginManager().registerEvents(new Listeners(), this);
-
+		
+		RegisteredServiceProvider<DiamondBankAPI> diamondBankAPIProvider = getServer().getServicesManager().getRegistration(DiamondBankAPI.class);
+		if (diamondBankAPIProvider == null) {
+			getLogger().severe("DiamondBank-OG API is null");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+		diamondBankAPI = diamondBankAPIProvider.getProvider();
 	}
 
 	public static TemplateOG getPlugin() {
-
 		return plugin;
-
 	}
 
+	public static DiamondBankAPI diamondBankAPI() {
+		return diamondBankAPI;
+	}
 }
