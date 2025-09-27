072dd77df0b178478e76e6c3953fc428f6dca88e
freetolga <freetolga@protonmail.com>
2025-09-27 12:10:32 +0000

    explain the code with comments

--- src/main/java/plugin/AnnouncementTask.java
+++ src/main/java/plugin/AnnouncementTask.java
@@ -5,17 +5,25 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
+// inherit BukkitRunnable to use the easier runTaskTimerAsyncronously method
 public class AnnouncementTask extends BukkitRunnable {
 
+	// getStringList returns a List of String's
     private List<String> announcements;
+    // current index of announcement
     private int index = 0;
+    // TODO: implement reloading
+    // last index of the announcements array
     private int lastIndex;
+    // interval of seconds between each announcement
     private long interval;
 
+    // constructor
     AnnouncementTask(List<String> announcements, long interval) {
 
         this.announcements = announcements;
         this.interval = interval;
+        // now that we have the List, populate lastIndex
         this.lastIndex = announcements.size();
 
     }
@@ -23,9 +31,12 @@ public class AnnouncementTask extends BukkitRunnable {
     @Override
     public void run() {
 
+    	// make the announcement once the task is run
         Bukkit.broadcast(UtilitiesOG.trueogExpand(announcements.get(index)));
+        // move on to the next announcement
         if (index < lastIndex - 1)
             index += 1;
+        // move on to the first announcement if we are at the last one
         else
             index = 0;
 
--- src/main/java/plugin/AnnouncementsOG.java
+++ src/main/java/plugin/AnnouncementsOG.java
@@ -1,5 +1,5 @@
 // This is free and unencumbered software released into the public domain.
-// Author: NotAlexNoyle.
+// Author: freetolga.
 package plugin;
 
 import java.util.List;
@@ -10,27 +10,47 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class AnnouncementsOG extends JavaPlugin {
 
     private static AnnouncementsOG plugin;
+    // store the pointer to the task to prepare for reloading support
     private static AnnouncementTask announcementTask;
     private static FileConfiguration config;
+    
+    // declare these inside the class to change them later when reloading is added
+    // might be reorganized later
     private static long timeInterval;
     private static List<String> announcements;
 
     @Override
     public void onEnable() {
 
+    	// get a pointer to the main plugin class to pass into other methods later
         plugin = this;
 
+        // initialize config with default value if it doesnt exist yet
         saveDefaultConfig();
+
+        // load the configuration from the server otherwise
         config = getConfig();
 
+        // get the value of these config.yml keys
         announcements = config.getStringList("announcements");
         timeInterval = config.getInt("timeInterval");
 
+        // populate the pointer to the task by creating an instance of it
         announcementTask = new AnnouncementTask(announcements, timeInterval);
-        announcementTask.runTaskTimerAsynchronously(plugin, timeInterval * 20, timeInterval * 20);
+
+        // call the method on the task to run it
+        announcementTask.runTaskTimerAsynchronously(
+        		// instance of our plugin
+        		plugin,
+        		// run every timeInterval seconds
+        		timeInterval * 20,
+        		// run for(or wait depending on how you look at it) every timeInterval seconds
+        		// before starting the task again
+        		timeInterval * 20);
 
     }
 
+    // TODO: fix onDisable and onReload
     @Override
     public void onDisable() {
 
@@ -38,12 +58,14 @@ public class AnnouncementsOG extends JavaPlugin {
 
     }
 
+    // boilerplate
     public static AnnouncementsOG getPlugin() {
 
         return plugin;
 
     }
 
+    // boilerplate
     public static FileConfiguration config() {
 
         return config;
